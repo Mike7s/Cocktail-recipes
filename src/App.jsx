ae8e95fc1c05cb14db765df91832d9e0866e635a
Mike7s <146244208+Mike7s@users.noreply.github.com>
2024-09-06 16:53:53 +0200

    add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card";
-import FavoritesBar from "./components/favList/favList";
-import Modal from "./modalFavBar";
 import ModalFavBar from "./modalFavBar";
 
 
